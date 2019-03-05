0df44729f4409b009ea09b8228408014d4e93824
many-states <45075374+many-states@users.noreply.github.com>
2019-03-05 12:19:00 -0500

    added all standard methods to top-level manager

--- metaroot/manager.py
+++ metaroot/manager.py
@@ -1,5 +1,5 @@
 from metaroot.config import locate_config
-from metaroot.utils import instantiate_object_from_class_path
+from metaroot.utils import instantiate_object_from_class_path, get_logger
 from metaroot.common import Result
 
 
@@ -11,66 +11,140 @@ class Manager:
         for hook in hooks:
             self._managers.append(instantiate_object_from_class_path(hook))
 
-    def add_account(self, account_atts) -> Result:
+    def add_group(self, group_atts: dict) -> Result:
         status = 0
-        response = ""
+        all_results = {}
         for manager in self._managers:
-            result = manager.add_account(account_atts)
+            result = manager.add_group(group_atts)
             status = status + result.status
-            response = response + "{0}={1}".format(manager.__class__.__name__, result.status)
-        return Result(status, response)
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
 
-    def add_user(self, user_atts) -> Result:
+    def get_group(self, name: str) -> Result:
         status = 0
-        response = ""
+        all_results = {}
+        for manager in self._managers:
+            result = manager.get_group(name)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def get_members(self, name: str) -> Result:
+        status = 0
+        all_results = {}
+        for manager in self._managers:
+            result = manager.get_members(name)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def update_group(self, group_atts: dict) -> Result:
+        status = 0
+        all_results = {}
+        for manager in self._managers:
+            result = manager.update_group(group_atts)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def delete_group(self, name: str) -> Result:
+        status = 0
+        all_results = {}
+        for manager in self._managers:
+            result = manager.delete_group(name)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def exists_group(self, name: str) -> Result:
+        status = 0
+        all_results = {}
+        for manager in self._managers:
+            result = manager.exists_group(name)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def add_user(self, user_atts: dict) -> Result:
+        status = 0
+        all_results = {}
         for manager in self._managers:
             result = manager.add_user(user_atts)
             status = status + result.status
-            response = response + "{0}={1}".format(manager.__class__.__name__, result.status)
-        return Result(status, response)
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
 
-    def set_user_default_account(self, user_name, account_name) -> Result:
+    def update_user(self, user_atts: dict) -> Result:
         status = 0
-        response = ""
+        all_results = {}
         for manager in self._managers:
-            result = manager.set_user_default_account(user_name, account_name)
+            result = manager.update_user(user_atts)
             status = status + result.status
-            response = response + "{0}={1}".format(manager.__class__.__name__, result.status)
-        return Result(status, response)
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
 
-    def delete_account(self, name) -> Result:
+    def get_user(self, name: str) -> Result:
         status = 0
-        response = ""
+        all_results = {}
         for manager in self._managers:
-            result = manager.delete_account(name)
+            result = manager.get_user(name)
             status = status + result.status
-            response = response + "{0}={1}".format(manager.__class__.__name__, result.status)
-        return Result(status, response)
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
 
-    def delete_user(self, name) -> Result:
+    def delete_user(self, name: str) -> Result:
         status = 0
-        response = ""
+        all_results = {}
         for manager in self._managers:
             result = manager.delete_user(name)
             status = status + result.status
-            response = response + "{0}={1}".format(manager.__class__.__name__, result.status)
-        return Result(status, response)
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
 
-    def associate_user_to_account(self, user_name, account_name) -> Result:
+    def exists_user(self, name: str) -> Result:
         status = 0
-        response = ""
+        all_results = {}
         for manager in self._managers:
-            result = manager.associate_user_to_account(user_name, account_name)
+            result = manager.exists_user(name)
             status = status + result.status
-            response = response + "{0}={1}".format(manager.__class__.__name__, result.status)
-        return Result(status, response)
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
 
-    def disassociate_user_from_account(self, user_name, account_name) -> Result:
+    def set_user_default_group(self, user_name: str, group_name: str) -> Result:
         status = 0
-        response = ""
+        all_results = {}
         for manager in self._managers:
-            result = manager.disassociate_user_from_account(user_name, account_name)
+            result = manager.set_user_default_group(user_name, group_name)
             status = status + result.status
-            response = response + "{0}={1}".format(manager.__class__.__name__, result.status)
-        return Result(status, response)
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def associate_user_to_group(self, user_name: str, group_name: str) -> Result:
+        status = 0
+        all_results = {}
+        for manager in self._managers:
+            result = manager.associate_user_to_group(user_name, group_name)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def disassociate_user_from_group(self, user_name: str, group_name: str) -> Result:
+        status = 0
+        all_results = {}
+        for manager in self._managers:
+            result = manager.disassociate_user_from_group(user_name, group_name)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    def disassociate_users_from_group(self, user_names: list, group_name: str) -> Result:
+        status = 0
+        all_results = {}
+        for manager in self._managers:
+            result = manager.disassociate_users_from_group(user_names, group_name)
+            status = status + result.status
+            all_results[manager.__class__.__name__] = result
+        return Result(status, all_results)
+
+    
 
