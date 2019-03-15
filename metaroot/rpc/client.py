2fda69fb4a56a7384902d8905fcacfc7f374e03f
many-states <45075374+many-states@users.noreply.github.com>
2019-03-15 14:48:31 -0400

    - Added connection recovery logic to send method - Refactored connection logic out of constructor

--- metaroot/rpc/client.py
+++ metaroot/rpc/client.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import pika
+import pika.exceptions
 import uuid
 import yaml
-from metaroot.config import Config
-import metaroot.utils
 from metaroot.common import Result
+from metaroot.config import Config
+from metaroot.utils import get_logger
 
 
 class RPCClient:
     """
-    A lightweight RPC client based on pika that passes YAML messages
+    An RPC client based on pika that passes YAML messages
     """
 
     def __init__(self, config: Config):
@@ -27,40 +28,79 @@ class RPCClient:
             If any underlying operations fail by raising an exception
 
         """
-        # Pretty standard connection stuff
-        credentials = pika.PlainCredentials(config.get_mq_user(), config.get_mq_pass())
-        parameters = pika.ConnectionParameters(host=config.get_mq_host(),
-                                               port=config.get_mq_port(),
-                                               virtual_host='/',
-                                               credentials=credentials,
-                                               heartbeat=30)
-        self.connection = pika.BlockingConnection(parameters)
-        self.channel = self.connection.channel()
-
-        # Declare a delete-on-exit queue for this client to receive RPC callback message
-        qd_result = self.channel.queue_declare(exclusive=True)
-        self.callback_queue = qd_result.method.queue
-
-        # Specify the function to process the RPC callback responses
-        self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
-
-        # Initialize remaining attributes
+        self.config = config
+        self.connection = None
+        self.channel = None
+        self.callback_queue = None
         self.corr_id = None
         self.response = None
-        self.queue = config.get_mq_queue_name()
-        self._logger = metaroot.utils.get_logger(RPCClient.__name__,
-                                                 config.get_log_file(),
-                                                 config.get_mq_file_verbosity(),
-                                                 config.get_mq_screen_verbosity())
+        self.queue = self.config.get_mq_queue_name()
+        self.logger = get_logger(RPCClient.__name__,
+                                 config.get_log_file(),
+                                 config.get_mq_file_verbosity(),
+                                 config.get_mq_screen_verbosity())
+        self.connect()
 
     def __del__(self):
         """
-        Attempt to close the pika connection in the destructor if the connection is still open
+        Attempt to shutdown cleanly by closing pika connection
+        """
+        self.close()
+
+    def _connection_blocked_cb(self, frame):
+        """
+        Callback to log cases where the server began rejecting messages
+        """
+        self.logger.WARN("The connection has been blocked by the server")
+
+    def _connection_unblocked_cb(self, frame):
+        """
+        Callback to log cases where the server resumed accepting messages after a period of rejecting them
+        """
+        self.logger.WARN("The connection has been unblocked")
+
+    def connect(self) -> bool:
+        """
+        Connect the RPC client to the message queue
+
+        Returns
+        ----------
+        bool
+            True if connection was successful, False otherwise
         """
         try:
-            self.connection.close()
+            # Pretty standard connection stuff
+            credentials = pika.PlainCredentials(self.config.get_mq_user(), self.config.get_mq_pass())
+            parameters = pika.ConnectionParameters(host=self.config.get_mq_host(),
+                                                   port=self.config.get_mq_port(),
+                                                   virtual_host='/',
+                                                   credentials=credentials,
+                                                   heartbeat=30)
+            self.connection = pika.BlockingConnection(parameters)
+            self.connection.add_on_connection_blocked_callback(self._connection_blocked_cb)
+            self.connection.add_on_connection_unblocked_callback(self._connection_unblocked_cb)
+            self.channel = self.connection.channel()
+
+            # Declare a delete-on-exit queue for this client to receive RPC callback message
+            qd_result = self.channel.queue_declare(exclusive=True)
+            self.callback_queue = qd_result.method.queue
+
+            # Specify the function to process the RPC callback responses
+            self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
+
+            # Set properties to track call/response to
+            self.corr_id = None
+            self.response = None
+
+            # Success
+            return True
+
         except Exception as e:
-            self._logger.warn("closing connection raised an exception")
+            self.logger.error("connection attempt threw exception")
+            self.logger.exception(e)
+
+            # Failure
+            return False
 
     def on_response(self, ch, method, props, body):
         """
@@ -85,13 +125,16 @@ class RPCClient:
         if self.corr_id == props.correlation_id:
             self.response = body
 
-    def finish(self):
+    def close(self):
         """
         Shutdown the RPC Client
         """
-        self.connection.close()
+        try:
+            self.connection.close()
+        except Exception as e:
+            self.logger.warn("closing connection raised an exception")
 
-    def call(self, obj) -> Result:
+    def send(self, obj) -> Result:
         """
         Method to initiate an RPC request
 
@@ -110,20 +153,35 @@ class RPCClient:
         try:
             message = yaml.safe_dump(obj)
         except yaml.YAMLError as exc:
-            self._logger.error("YAML serialization error: %s", exc)
-            self._logger.error("{0}".format(obj))
+            self.logger.error("YAML serialization error: %s", exc)
+            self.logger.error("{0}".format(obj))
             return Result(453, None)
 
         self.response = None
         self.corr_id = str(uuid.uuid4())
 
         # Send RPC request to server
-        self.channel.basic_publish(exchange='',
-                                   routing_key=self.queue,
-                                   body=message,
-                                   properties=pika.BasicProperties(
-                                       reply_to=self.callback_queue,
-                                       correlation_id=self.corr_id))
+        not_sent = True
+        attempts = 1
+        while not_sent and attempts < 10:
+            try:
+                self.channel.basic_publish(exchange='',
+                                           routing_key=self.queue,
+                                           body=message,
+                                           properties=pika.BasicProperties(
+                                               reply_to=self.callback_queue,
+                                               correlation_id=self.corr_id))
+                not_sent = False
+            except pika.exceptions.ConnectionClosed as e:
+                self.logger.error("Failed to send on attempt %d because connection closed. Reconnecting...", attempts)
+                self.connect()
+            except Exception as e:
+                self.logger.error("Failed to send on attempt %d. Retrying...", attempts)
+
+            attempts = attempts + 1
+        if not_sent:
+            self.logger.error("Failed to deliver message %s:%s", self.queue, message.rstrip())
+            return Result(470, "Message could not be delivered")
 
         # Wait for response
         while self.response is None:
@@ -135,8 +193,8 @@ class RPCClient:
             res_obj = yaml.safe_load(self.response)
             return Result.from_transport_format(res_obj)
         except yaml.YAMLError as exc:
-            self._logger.error("YAML serialization error: %s", exc)
-            self._logger.error("{0}".format(obj))
+            self.logger.error("YAML serialization error: %s", exc)
+            self.logger.error("{0}".format(obj))
             return Result(454, None)
 
 
