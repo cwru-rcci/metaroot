5b2c2387007353d0bd7bb0187099a25ddcc1fb60
many-states <45075374+many-states@users.noreply.github.com>
2019-03-15 14:44:01 -0400

    Re-generated to align with changes in config location. Includes pydoc comments pointing to the SlurmManager methods for each RPC equivalent

--- metaroot/slurm/manager_rpc.py
+++ metaroot/slurm/manager_rpc.py
@@ -1,107 +1,226 @@
 from metaroot.rpc.client import RPCClient
-from metaroot.config import locate_config
+from metaroot.config import get_config
 from metaroot.common import Result
 
 
 class SlurmManager(RPCClient):
     """
     RPC wrapper for metaroot.slurm.manager.SlurmManager
-    Auto-generated 2019-03-05T10:07:07.183258
+    Auto-generated 2019-03-14T15:22:48.248287
     """
     def __init__(self):
-        super().__init__(locate_config(self.__class__.__name__))
+        super().__init__(get_config(self.__class__.__name__))
 
-    def add_group(self, account_atts) -> Result:
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.add_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.add_group
+    """
+    def add_group(self, group_atts) -> Result:
         request = {'action': 'add_group',
-                   'account_atts': account_atts,
+                   'class': 'metaroot.slurm.manager.SlurmManager',
+                   'group_atts': group_atts,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.add_user
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.add_user
+    """
     def add_user(self, user_atts) -> Result:
         request = {'action': 'add_user',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'user_atts': user_atts,
                    }
-        return self.call(request)
+        return self.send(request)
 
-    def associate_user_to_group(self, user_name, account_name) -> Result:
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.associate_user_to_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.associate_user_to_group
+    """
+    def associate_user_to_group(self, user_name, group_name) -> Result:
         request = {'action': 'associate_user_to_group',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'user_name': user_name,
-                   'account_name': account_name,
+                   'group_name': group_name,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.delete_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.delete_group
+    """
     def delete_group(self, name) -> Result:
         request = {'action': 'delete_group',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'name': name,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.delete_user
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.delete_user
+    """
     def delete_user(self, name) -> Result:
         request = {'action': 'delete_user',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'name': name,
                    }
-        return self.call(request)
+        return self.send(request)
 
-    def disassociate_user_from_group(self, user_name, account_name) -> Result:
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.disassociate_user_from_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.disassociate_user_from_group
+    """
+    def disassociate_user_from_group(self, user_name, group_name) -> Result:
         request = {'action': 'disassociate_user_from_group',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'user_name': user_name,
-                   'account_name': account_name,
+                   'group_name': group_name,
                    }
-        return self.call(request)
+        return self.send(request)
 
-    def disassociate_users_from_group(self, user_names, account_name) -> Result:
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.disassociate_users_from_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.disassociate_users_from_group
+    """
+    def disassociate_users_from_group(self, user_names, group_name) -> Result:
         request = {'action': 'disassociate_users_from_group',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'user_names': user_names,
-                   'account_name': account_name,
+                   'group_name': group_name,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.exists_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.exists_group
+    """
     def exists_group(self, name) -> Result:
         request = {'action': 'exists_group',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'name': name,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.exists_user
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.exists_user
+    """
     def exists_user(self, name) -> Result:
         request = {'action': 'exists_user',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'name': name,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.get_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.get_group
+    """
     def get_group(self, name) -> Result:
         request = {'action': 'get_group',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'name': name,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.get_members
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.get_members
+    """
     def get_members(self, name) -> Result:
         request = {'action': 'get_members',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'name': name,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.get_user
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.get_user
+    """
     def get_user(self, name) -> Result:
         request = {'action': 'get_user',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'name': name,
                    }
-        return self.call(request)
+        return self.send(request)
 
-    def set_user_default_group(self, user_name, account_name) -> Result:
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.set_user_default_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.set_user_default_group
+    """
+    def set_user_default_group(self, user_name, group_name) -> Result:
         request = {'action': 'set_user_default_group',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'user_name': user_name,
-                   'account_name': account_name,
+                   'group_name': group_name,
                    }
-        return self.call(request)
+        return self.send(request)
 
-    def update_group(self, account_atts) -> Result:
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.update_group
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.update_group
+    """
+    def update_group(self, group_atts) -> Result:
         request = {'action': 'update_group',
-                   'account_atts': account_atts,
+                   'class': 'metaroot.slurm.manager.SlurmManager',
+                   'group_atts': group_atts,
                    }
-        return self.call(request)
+        return self.send(request)
 
+    """
+    An RPC wrapper to the method metaroot.slurm.manager.SlurmManager.update_user
+
+    See Also
+    --------
+    metaroot.slurm.manager.SlurmManager.update_user
+    """
     def update_user(self, user_atts) -> Result:
         request = {'action': 'update_user',
+                   'class': 'metaroot.slurm.manager.SlurmManager',
                    'user_atts': user_atts,
                    }
-        return self.call(request)
-
+        return self.send(request)
