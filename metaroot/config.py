f35a02d8867b01fa3152f1d868cfe1369fd0af5d
many-states <45075374+many-states@users.noreply.github.com>
2019-03-15 14:36:53 -0400

    - Updated enum parameters so that name=value - Refactored method names to better reflect action - Added singleton 'auto' to hold configuration loaded from file - Adjusted strategy for get_config to make the search for configuration   simpler and more consistent

--- metaroot/config.py
+++ metaroot/config.py
@@ -4,23 +4,39 @@ from enum import Enum
 
 
 class ConfigParams(Enum):
-    MQUSER = 'METAROOT_MQUSER'
-    MQPASS = 'METAROOT_MQPASS'
-    MQHOST = 'METAROOT_MQHOST'
-    MQPORT = 'METAROOT_MQPORT'
-    MQNAME = 'METAROOT_MQNAME'
-    MQHDLR = 'METAROOT_MQHDLR'
-    MQSVER = 'METAROOT_MQSVER'
-    MQFVER = 'METAROOT_MQFVER'
-    MRLOGF = 'METAROOT_LOGFILE'
-    MRHOOK = 'METAROOT_HOOKS'
+    """
+    Named parameters used for mapping configuration file content to getters
+    """
+    METAROOT_MQUSER = 'METAROOT_MQUSER'
+    METAROOT_MQPASS = 'METAROOT_MQPASS'
+    METAROOT_MQHOST = 'METAROOT_MQHOST'
+    METAROOT_MQPORT = 'METAROOT_MQPORT'
+    METAROOT_MQNAME = 'METAROOT_MQNAME'
+    METAROOT_MQHDLR = 'METAROOT_MQHDLR'
+    METAROOT_SCREEN_VERBOSITY = 'METAROOT_SCREEN_VERBOSITY'
+    METAROOT_FILE_VERBOSITY = 'METAROOT_FILE_VERBOSITY'
+    METAROOT_LOG_FILE = 'METAROOT_LOG_FILE'
+    METAROOT_HOOKS = 'METAROOT_HOOKS'
+    METAROOT_ACTIVITY_STREAM = 'METAROOT_ACTIVITY_STREAM'
+    METAROOT_DATABASE = 'METAROOT_DATABASE'
+
 
 class Config:
+    """
+    A standardized source of configuration information that maps a dict of key=value pairs to getters
+    """
 
     def __init__(self):
         self._data = dict()
-        self._data[ConfigParams.MQSVER.value] = "INFO"
-        self._data[ConfigParams.MQFVER.value] = "INFO"
+        self._data[ConfigParams.METAROOT_LOG_FILE.value] = "metaroot.log"
+        self._data[ConfigParams.METAROOT_SCREEN_VERBOSITY.value] = "INFO"
+        self._data[ConfigParams.METAROOT_FILE_VERBOSITY.value] = "INFO"
+
+    def get(self, key):
+        return self._data[key]
+
+    def has(self, key):
+        return key in self._data
 
     def populate(self, atts: dict):
         valid_properties = set(item.value for item in ConfigParams)
@@ -28,75 +44,79 @@ class Config:
             if prop in valid_properties:
                 self._data[prop] = atts[prop]
 
-    def load(self, file):
-        stream = open(file, 'r')
-        cfg_obj = yaml.load(stream)
-        stream.close()
-        self.populate(cfg_obj)
-
-    def get(self, key):
-        return self._data[key]
-
     def get_mq_user(self):
-        return self._data[ConfigParams.MQUSER.value]
+        return self._data[ConfigParams.METAROOT_MQUSER.value]
 
     def get_mq_pass(self):
-        return self._data[ConfigParams.MQPASS.value]
+        return self._data[ConfigParams.METAROOT_MQPASS.value]
 
     def get_mq_host(self):
-        return self._data[ConfigParams.MQHOST.value]
+        return self._data[ConfigParams.METAROOT_MQHOST.value]
 
     def get_mq_port(self):
-        return int(self._data[ConfigParams.MQPORT.value])
+        return int(self._data[ConfigParams.METAROOT_MQPORT.value])
 
     def get_mq_queue_names(self):
-        return self._data[ConfigParams.MQNAME.value]
+        return self._data[ConfigParams.METAROOT_MQNAME.value]
 
     def get_mq_queue_name(self):
-        if isinstance(self._data[ConfigParams.MQNAME.value], list):
-            return self._data[ConfigParams.MQNAME.value][0]
+        if isinstance(self._data[ConfigParams.METAROOT_MQNAME.value], list):
+            return self._data[ConfigParams.METAROOT_MQNAME.value][0]
         else:
-            return self._data[ConfigParams.MQNAME.value]
+            return self._data[ConfigParams.METAROOT_MQNAME.value]
 
     def get_mq_handler_class(self):
-        return self._data[ConfigParams.MQHDLR.value]
+        return self._data[ConfigParams.METAROOT_MQHDLR.value]
 
     def get_mq_screen_verbosity(self):
-        return self._data[ConfigParams.MQSVER.value]
+        return self._data[ConfigParams.METAROOT_SCREEN_VERBOSITY.value]
 
     def get_mq_file_verbosity(self):
-        return self._data[ConfigParams.MQFVER.value]
+        return self._data[ConfigParams.METAROOT_FILE_VERBOSITY.value]
 
     def get_log_file(self):
-        return self._data[ConfigParams.MRLOGF.value]
+        return self._data[ConfigParams.METAROOT_LOG_FILE.value]
 
     def get_hooks(self):
-        return self._data[ConfigParams.MRHOOK.value]
+        return self._data[ConfigParams.METAROOT_HOOKS.value]
+
+    def get_activity_stream(self):
+        return self._data[ConfigParams.METAROOT_ACTIVITY_STREAM.value]
+
+    def get_activity_stream_db(self):
+        return self._data[ConfigParams.METAROOT_DATABASE.value]
 
 
-def locate_config(clazz: str):
+def get_config(key: str):
     """
-    The classes that are run via RPC are meant to be interchangeable by simply changing the import statement. To
-    accomplish that we provide this utility method that subclasses of RPC Client call in their constructor to locate
-    the class level configuration. E.g., this would tell a RPC client subclass where the RPC server is running,
-    credentials to connect and what "channel" to publish RPC requests to.
+    Returns environment specific information by searching a set of locations. The current implementation searches for
+    METAROOT_'key' in the following:
+        - The top-level keys of the global 'auto' which is loaded from local file 'metaroot[-test].yaml' if it exists
+        - The attributes of django.conf.settings
+
+    The returned configuration is created by augmenting/overwriting the parameters defined for key METAROOT_GLOBAL
+    with all parameters defined for METAROOT_'key'.
+
+    The convention used throughout the project is that class pass their class name as 'key'. E.g.
+    metaroot.slurm.manager_rpc.SlurmManager calls locate_config("SlurmManager") which searches for the key
+    METAROOT_SLURMMANAGER.
 
     Parameters
     ----------
-    clazz: str
-        The class as a dot delimited string
+    key: str
+        The key that identifies the configuration information of interest
 
     Returns
     ----------
     Config
-        The configuration for this class
+        The configuration if it is found
 
     Raises
     ----------
     Exception
         If no configuration could be found, or the configuration information was invalid
     """
-    key = "METAROOT_"+clazz.upper()
+    key = "METAROOT_" + key.upper()
     config = Config()
 
     # #
@@ -106,32 +126,94 @@ def locate_config(clazz: str):
     # #
     ready = False
 
+    # #
     #
-
+    # Check for config file loaded by
+    #
+    # #
     try:
-        from django.conf import settings
-        config.populate(getattr(settings, key))
+        config.populate(auto["METAROOT_GLOBAL"])
+        config.populate(auto[key])
         ready = True
+        print("{0} using auto-configuration from global".format(key))
     except Exception:
         pass
 
     # #
     #
-    # If no in-memory settings found, check on the disk. If we end up here, we will print some diagnostic information if
-    # we did not find the file
+    # Check if running in Django
     #
     # #
     if not ready:
-        file_name = clazz + "RPC.yml"
         try:
-            config.load(file_name)
+            from django.conf import settings
+            config.populate(getattr(settings, "METAROOT_GLOBAL"))
+            config.populate(getattr(settings, key))
             ready = True
-        except FileNotFoundError:
-            print("Could not locate RPC server config {0}/{1}".format(os.getcwd(), file_name))
-        except yaml.YAMLError:
-            print("Could not parse {0}/{1} as YAML".format(os.getcwd(), file_name))
+            print("{0} using configuration from DJango settings".format(key))
+        except Exception:
+            pass
 
     if ready:
         return config
     else:
         raise Exception("Could not locate configuration in any standard locations")
+
+
+def load_file_based_config():
+    """
+    Locates environment specific information by searching for file 'metaroot[-test].yaml' starting in the current
+    working directory and traversing upward no more than four levels. metaroot-test.yaml takes precedence over
+    metaroot.yaml if they are both located at the same level.
+
+    Returns
+    ----------
+    Config
+        The contents of the YAML file as an object if it was located, and None otherwise
+    """
+    path = ""
+    ready = False
+    attempts = 0
+
+    while not ready and attempts < 4:
+        try:
+            # Test config takes precedence over production config
+            config_file="metaroot.yaml"
+            if os.path.exists(path+"metaroot-test.yaml"):
+                print("Using configuration, metaroot-test.yaml. You should rename or remove this file in production")
+                config_file = "metaroot-test.yaml"
+
+            stream = open(path+config_file, 'r')
+            config = yaml.safe_load(stream)
+            stream.close()
+            print("Loaded first configuration file found at {0}\n".format(path+config_file))
+            return config
+        except Exception:
+            if os.path.exists(path + "../"):
+                path = path + "../"
+                attempts = attempts + 1
+            else:
+                break
+    return None
+
+
+def get_global_config():
+    """
+    Retrieves only the global parameters.
+
+    Returns
+    ----------
+    Config
+        The configuration if it is found
+
+    Raises
+    ----------
+    Exception
+        If no configuration could be found, or the configuration information was invalid
+    """
+    config = Config()
+    config.populate(auto["METAROOT_GLOBAL"])
+    return config
+
+
+auto = load_file_based_config()
