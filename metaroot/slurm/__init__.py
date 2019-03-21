abc3b750e4d34e28f879970df33487a3dfb27471
many-states <45075374+many-states@users.noreply.github.com>
2019-03-21 14:50:29 -0400

    Migrated SLURM manager to a separate package

--- metaroot/slurm/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-This module provides classes for local and remote management of a SLURM database. It requires the command line utility
-sacctmgr be available, and the process to be run with sufficient privilege.
-"""
