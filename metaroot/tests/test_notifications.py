52fbd17b53e9ccb3751e6dabedda71fe32d4c3b2
many-states <45075374+many-states@users.noreply.github.com>
2019-05-21 12:01:19 -0400

    add main to run tests form command line

--- metaroot/tests/test_notifications.py
+++ metaroot/tests/test_notifications.py
@@ -5,3 +5,8 @@ from metaroot.api.notifications import send_email
 class NotificationsTest(unittest.TestCase):
     def test_send_email_fail_address_unresolveable(self):
         self.assertEqual(False, send_email("foo", "test email", "<i>Test Content</i>"))
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(NotificationsTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
