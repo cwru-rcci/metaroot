372f9ddc653c52018a9ab5a4ce3ddbc512979d9c
many-states <45075374+many-states@users.noreply.github.com>
2019-05-21 12:00:50 -0400

    add tests for config specified through environment

---
+++ metaroot/tests/test_env_file_config.py
@@ -0,0 +1,19 @@
+import unittest
+import metaroot.config
+
+
+class MetarootEnvfileConfigTests(unittest.TestCase):
+
+    def test_envfile_global_config(self):
+        config = metaroot.config.get_global_config()
+        self.assertEqual("$NONE", config.get_log_file())
+
+    def test_envfile_class_config(self):
+        config = metaroot.config.get_config("ROUTER")
+        self.assertEqual(["metaroot.tests.test_router.Handler1", "metaroot.tests.test_router.Handler2"],
+                         config.get_hooks())
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(MetarootEnvfileConfigTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
